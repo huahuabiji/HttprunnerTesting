02191a147cebe952373e1f9a504a55a8e3fd8539
lijian02 <lijian02@baijiahulian.com>
2020-03-21 08:47:44 +0800

    定义和调用外部函数

--- debugtalk.py
+++ debugtalk.py
@@ -3,3 +3,10 @@ import time
 
 def sleep(n_secs):
     time.sleep(n_secs)
+
+def gettime_stamp():  # 订单方法获取时间戳
+    return str(int(time.time() * 1000))
+
+
+def print_documentId(doc_id): # 打印documentId参数里边的值
+    print ("documentId = {}".format(doc_id))
