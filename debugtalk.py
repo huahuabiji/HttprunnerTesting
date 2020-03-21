37dd8e83add28aae300eeb9dc6fc624fa23f639a
lijian02 <lijian02@baijiahulian.com>
2020-03-21 09:11:05 +0800

    base_url可自定义脚本执行环境

--- debugtalk.py
+++ debugtalk.py
@@ -10,3 +10,11 @@ def gettime_stamp():  # 订单方法获取时间戳
 
 def print_documentId(doc_id): # 打印documentId参数里边的值
     print ("documentId = {}".format(doc_id))
+
+def get_base_url(env_type="prod"):  # 定义环境函数
+    if env_type == "prod": # 线上环境地址
+        return "https://mubu.com"
+    elif env_type == "beta":  # beta环境地址
+        return "https://beta-mubu.com"
+    else:
+        return "https://test-mubu.com"  # 测试环境地址
